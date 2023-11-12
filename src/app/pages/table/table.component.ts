b17054307068cbbb3df4ece6073af6f72b3d3cdd
Алкс Сид <alxs1dd93@gmail.com>
2023-11-12 23:41:23 +0700

    fix bug a previous commit

--- src/app/pages/table/table.component.ts
+++ src/app/pages/table/table.component.ts
@@ -4,7 +4,7 @@ import { Table } from 'src/app/interface/table';
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
-  styleUrls: ['./table.component.scss']
+  styleUrls: ['./table.component.scss'],
 })
 export class TableComponent implements OnInit {
   headerRoadMapTable: string[] = ['technology', 'theme', 'bool'];
@@ -14,42 +14,93 @@ export class TableComponent implements OnInit {
   ngOnInit(): void {
     this.loadLocalStorage();
   }
+
   roadmapTable: Table[] = [
-    { technology: 'HTML', theme: 'Элементы в HTML', bool: false},
-    { technology: '', theme: 'Формы, валидация форм', bool: false},
-    { technology: '', theme: 'Семантическая верстка', bool: false},
-    { technology: 'CSS', theme: 'Элементы в HTML', bool: false},
-    { technology: '', theme: 'Селекторы', bool: false},
-    { technology: '', theme: 'Свойства', bool: false},
-    { technology: '', theme: 'Позиционирование элементов, Flexbox, Grid', bool: false},
-    { technology: '', theme: 'Трансформации, переходы, анимации', bool: false},
-    { technology: '', theme: '‘Адаптивный дизайн и медиазапросы', bool: false},
-    { technology: '', theme: 'СSS-препроцессоры(sass, scss, less)', bool: false},
-    { technology: '', theme: 'БЭМ', bool: false},
-    { technology: 'JavaScript', theme: 'Типы данных, преобразования типов', bool: false},
-    { technology: '', theme: 'Условное ветвление, логические операторы, циклы', bool: false},
-    { technology: '', theme: 'Функции, функциональные выражения, стрелочные функции, поднятие', bool: false},
-    { technology: '', theme: 'Замыкание, IIFE', bool: false},
-    { technology: '', theme: 'Строки, шаблонные строки, регулярные выражения', bool: false},
-    { technology: '', theme: 'Массивы, методы массивов, перебор массивов', bool: false},
-    { technology: '', theme: 'Объекты, методы объектов, сравнение объектов, ссылки', bool: false},
-    { technology: '', theme: 'Классы, наследование, статические свойства и методы, защищенные свойства и методы', bool: false},
-    { technology: '', theme: 'Колбэки, промисы, обработка ошибок, микротаски, азупс/амаи, event loop', bool: false},
-    { technology: '', theme: 'Взаимодействие DOM (создание, добавление, изменение и удаление элементов веб-станиць), браузерные события, распространение событий', bool: false},
-    { technology: '', theme: 'Хранение данных - сооке, session storage, local storage', bool: false},
-    { technology: '', theme: 'Дебаг в Chrome DevTools', bool: false},
-    { technology: '', theme: 'XMLHttpRequest, FetchApi, WebSocket', bool: false},
+    { technology: 'HTML', theme: 'Элементы в HTML', bool: false },
+    { technology: '', theme: 'Формы, валидация форм', bool: false },
+    { technology: '', theme: 'Семантическая верстка', bool: false },
+    { technology: 'CSS', theme: 'Элементы в HTML', bool: false },
+    { technology: '', theme: 'Селекторы', bool: false },
+    { technology: '', theme: 'Свойства', bool: false },
+    {
+      technology: '',
+      theme: 'Позиционирование элементов, Flexbox, Grid',
+      bool: false,
+    },
+    { technology: '', theme: 'Трансформации, переходы, анимации', bool: false },
+    { technology: '', theme: '‘Адаптивный дизайн и медиазапросы', bool: false },
+    {
+      technology: '',
+      theme: 'СSS-препроцессоры(sass, scss, less)',
+      bool: false,
+    },
+    { technology: '', theme: 'БЭМ', bool: false },
+    {
+      technology: 'JavaScript',
+      theme: 'Типы данных, преобразования типов',
+      bool: false,
+    },
+    {
+      technology: '',
+      theme: 'Условное ветвление, логические операторы, циклы',
+      bool: false,
+    },
+    {
+      technology: '',
+      theme: 'Функции, функциональные выражения, стрелочные функции, поднятие',
+      bool: false,
+    },
+    { technology: '', theme: 'Замыкание, IIFE', bool: false },
+    {
+      technology: '',
+      theme: 'Строки, шаблонные строки, регулярные выражения',
+      bool: false,
+    },
+    {
+      technology: '',
+      theme: 'Массивы, методы массивов, перебор массивов',
+      bool: false,
+    },
+    {
+      technology: '',
+      theme: 'Объекты, методы объектов, сравнение объектов, ссылки',
+      bool: false,
+    },
+    {
+      technology: '',
+      theme:
+        'Классы, наследование, статические свойства и методы, защищенные свойства и методы',
+      bool: false,
+    },
+    {
+      technology: '',
+      theme:
+        'Колбэки, промисы, обработка ошибок, микротаски, азупс/амаи, event loop',
+      bool: false,
+    },
+    {
+      technology: '',
+      theme:
+        'Взаимодействие DOM (создание, добавление, изменение и удаление элементов веб-станиць), браузерные события, распространение событий',
+      bool: false,
+    },
+    {
+      technology: '',
+      theme: 'Хранение данных - сооке, session storage, local storage',
+      bool: false,
+    },
+    { technology: '', theme: 'Дебаг в Chrome DevTools', bool: false },
+    {
+      technology: '',
+      theme: 'XMLHttpRequest, FetchApi, WebSocket',
+      bool: false,
+    },
   ];
 
-  addLocalStorage() {
-    this.roadmapTable.forEach((item) => {
-      if (item.bool === true) {
-        localStorage.setItem(item.theme, 'true');
-      } else {
-        localStorage.removeItem(item.theme);
-      }
-    });
-  }
+addLocalStorage(index: number) {
+    const item = this.roadmapTable[index];
+    localStorage.setItem(item.theme, String(item.bool));
+}
 
   clearLocalStorage() {
     localStorage.clear();
@@ -75,4 +126,7 @@ export class TableComponent implements OnInit {
     console.log(this.local.split(','));
   }
 
+  refresh(): void {
+    window.location.reload();
+  }
 }
