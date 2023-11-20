cd4a37720ebf5b6648be559da1772f05b197a74d
alxs1dd93 <alxs1dd93@gmail.com>
2023-11-21 04:44:24 +0700

    add items roadmap table

--- src/app/pages/table/table.component.ts
+++ src/app/pages/table/table.component.ts
@@ -94,6 +94,267 @@ export class TableComponent implements OnInit {
       theme: 'XMLHttpRequest, FetchApi, WebSocket',
       bool: false,
     },
+    {
+      technology: 'Angular',
+      theme: 'Компоненты, модули, загрузка приложения',
+      bool: false,
+    },
+    {
+      technology: '',
+      theme: 'Привязка данных',
+      bool: false,
+    },
+    {
+      technology: '',
+      theme: 'Привязка к событиям дочернего компонента',
+      bool: false,
+    },
+    {
+      technology: '',
+      theme: 'Жизненный цикл компонента',
+      bool: false,
+    },
+    {
+      technology: '',
+      theme: 'Атрибутивные и структурные директивы, создание директив',
+      bool: false,
+    },
+    {
+      technology: '',
+      theme: 'Сервисы и dependency injection',
+      bool: false,
+    },
+    {
+      technology: '',
+      theme: 'Работа с формами',
+      bool: false,
+    },
+    {
+      technology: '',
+      theme: 'НТТР и взаимодействие с сервером',
+      bool: false,
+    },
+    {
+      technology: '',
+      theme: 'Маршрутизация',
+      bool: false,
+    },
+    {
+      technology: '',
+      theme: 'Pipes',
+      bool: false,
+    },
+    {
+      technology: 'RxJS',
+      theme: 'Observable',
+      bool: false,
+    },
+    {
+      technology: '',
+      theme: 'Observer',
+      bool: false,
+    },
+    {
+      technology: '',
+      theme: 'Subject',
+      bool: false,
+    },
+    {
+      technology: '',
+      theme: 'Scheduler',
+      bool: false,
+    },
+    {
+      technology: '',
+      theme: 'Subscription',
+      bool: false,
+    },
+    {
+      technology: '',
+      theme: 'Операторы и метод pipe()',
+      bool: false,
+    },
+    {
+      technology: 'NgRX',
+      theme: 'Actions',
+      bool: false,
+    },
+    {
+      technology: '',
+      theme: 'Reducers',
+      bool: false,
+    },
+    {
+      technology: '',
+      theme: 'Store',
+      bool: false,
+    },
+    {
+      technology: '',
+      theme: 'Selectors',
+      bool: false,
+    },
+    {
+      technology: '',
+      theme: 'Effects',
+      bool: false,
+    },
+    {
+      technology: '',
+      theme: 'Router store',
+      bool: false,
+    },
+    {
+      technology: '',
+      theme: 'Meta reducers',
+      bool: false,
+    },
+    {
+      technology: '',
+      theme: 'Entity state',
+      bool: false,
+    },
+    {
+      technology: 'TypeScript',
+      theme: 'Типы данных',
+      bool: false,
+    },
+    {
+      technology: '',
+      theme: 'Типы функции, стрелочные функции',
+      bool: false,
+    },
+    {
+      technology: '',
+      theme: 'Массивы, методы массивов, перебор массивов',
+      bool: false,
+    },
+    {
+      technology: '',
+      theme: 'Объекты',
+      bool: false,
+    },
+    {
+      technology: '',
+      theme: 'Перечисления enum',
+      bool: false,
+    },
+    {
+      technology: '',
+      theme: 'Кортежи',
+      bool: false,
+    },
+    {
+      technology: '',
+      theme:
+        'Классы, наследование, модификаторы доступа, методы доступа get и set, статические поля и методы',
+      bool: false,
+    },
+    {
+      technology: '',
+      theme: 'Интерфейсы, превдонимы обобщения',
+      bool: false,
+    },
+    {
+      technology: '',
+      theme: 'Миксины, декораторы',
+      bool: false,
+    },
+    {
+      technology: '',
+      theme: 'Содание и работа с модулями, пространство имен',
+      bool: false,
+    },
+    {
+      technology: 'Тестирование',
+      theme: 'е2е',
+      bool: false,
+    },
+    {
+      technology: '',
+      theme: 'Cypress',
+      bool: false,
+    },
+    {
+      technology: 'ООП',
+      theme: 'Принципы ООП(Инкапсуляция, абстранция, полиморфизм, наследование',
+      bool: false,
+    },
+    {
+      technology: '',
+      theme: 'Паттерны проектирования',
+      bool: false,
+    },
+    {
+      technology: '',
+      theme: 'Принципы SOLID',
+      bool: false,
+    },
+    {
+      technology: 'Остальное',
+      theme: 'Сборщики(Gulp, Webpack)',
+      bool: false,
+    },
+    {
+      technology: '',
+      theme: 'Пакетные менеджеры (npm)',
+      bool: false,
+    },
+    {
+      technology: '',
+      theme: 'Форматтеры (Prettier)',
+      bool: false,
+    },
+    {
+      technology: '',
+      theme: 'Компиляторы (Babel)',
+      bool: false,
+    },
+    {
+      technology: '',
+      theme: 'Линтеры (ESLint)',
+      bool: false,
+    },
+    {
+      technology: 'Безопасность',
+      theme: 'CORS',
+      bool: false,
+    },
+    {
+      technology: '',
+      theme: 'XSS',
+      bool: false,
+    },
+    {
+      technology: '',
+      theme: 'Content Security Policy',
+      bool: false,
+    },
+    {
+      technology: '',
+      theme: 'OWASP Security Risks',
+      bool: false,
+    },
+    {
+      technology: 'Мобильная разработка',
+      theme: 'Flutter',
+      bool: false,
+    },
+    {
+      technology: '',
+      theme: 'Ionic',
+      bool: false,
+    },
+    {
+      technology: 'Десктопная разработка',
+      theme: 'Electron',
+      bool: false,
+    },
+    {
+      technology: 'NodeJS',
+      theme: 'Основы',
+      bool: false,
+    },
   ];
 
   addLocalStorage(index: number) {
