8a0eaceb0dc0175f64c421f0971352d6811269e3
alxs1dd93 <alxs1dd93@gmail.com>
2023-11-21 08:20:22 +0700

    add footer, menu component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatTableModule} from '@angular/material/table';
+import { MatTableModule } from '@angular/material/table';
 import { MainComponent } from './pages/main/main.component';
 import { ErrorComponent } from './pages/error/error.component';
-import {MatCheckboxModule} from '@angular/material/checkbox';
-import {FormsModule} from '@angular/forms';
-import {MatButtonModule} from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 import { ToTopButtonComponent } from './ui/to-top-button/to-top-button.component';
 import { LayoutComponent } from './pages/layout/layout.component';
-import {MatIconModule} from '@angular/material/icon';
+import { MatIconModule } from '@angular/material/icon';
 import { HttpClientModule } from '@angular/common/http';
 import { RoadTableComponent } from './pages/road-table/road-table.component';
+import { MenuComponent } from './pages/menu/menu.component';
+import { FooterComponent } from './pages/footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -22,7 +24,9 @@ import { RoadTableComponent } from './pages/road-table/road-table.component';
     RoadTableComponent,
     LayoutComponent,
     ErrorComponent,
-     ToTopButtonComponent,
+    ToTopButtonComponent,
+    MenuComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
@@ -33,9 +37,9 @@ import { RoadTableComponent } from './pages/road-table/road-table.component';
     FormsModule,
     MatButtonModule,
     MatIconModule,
-    HttpClientModule
+    HttpClientModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
